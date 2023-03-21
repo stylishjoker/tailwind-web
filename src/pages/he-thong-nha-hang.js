fb82fc90e56c10113f48c3261f75a96b863534b7
stylishjoker <animetplink@gmail.com>
2023-03-21 12:14:43 +0700

    !

--- src/pages/he-thong-nha-hang.js
+++ src/pages/he-thong-nha-hang.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 const HeThongNhaHang = () => {
+	const defaultProps = {
+		center: {
+			lat: 21,
+			lng: 105,
+		},
+		zoom: 11,
+	};
+
 	return (
-		<div>
-			<h2>HeThongNhaHang</h2>
+		// Important! Always set the container height explicitly
+		<div style={{ height: '100vh', width: '100%' }} className="pt-[110px]">
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: '' }}
+				defaultCenter={defaultProps.center}
+				defaultZoom={defaultProps.zoom}>
+				<AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+			</GoogleMapReact>
 		</div>
 	);
 };
